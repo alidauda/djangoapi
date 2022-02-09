2e6b90883b554e275d6558453d2c8d9282c5d19f
alidauda <alidauda14@gmail.com>
2022-02-09 14:16:54 +0100

    get product api

--- api/views.py
+++ api/views.py
@@ -8,29 +8,36 @@ from .models import Users
 
 
 @api_view(['GET'])
-def getUser(request,pk):
-    
+def getUser(request, pk):
+
     user = Users.objects.get(id=pk)
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
-    
 
 @api_view(['POST'])
 def createUser(request):
     data = request.data
     createUser = Users.objects.create(
-            shopname=data['shopname'],
-            email=data['email']
-        )
+        shopname=data['shopname'],
+        email=data['email']
+    )
     serializer = UserSerializer(createUser, many=False)
     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def getProducts(request,pk):
-    print(pk)
-    user = Users.objects.get(id=pk)
-    products =user.product_set.all()
-    serializer=ProductSerializer(products, many=True)
-    return Response(serializer.data)
+@api_view(['GET','POST'])
+def getProducts(request, pk):
+    if request.method == 'GET':
+        user = Users.objects.get(id=pk)
+        products = user.product_set.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        data = request.data
+        user = Users.objects.get(id=pk)
+        products = user.product_set.create(
+            productName=data['productName'], price=data['price'], quantity=data['quantity'])
+
+        serializer = ProductSerializer(products, many=False)
+        return Response(serializer.data)
